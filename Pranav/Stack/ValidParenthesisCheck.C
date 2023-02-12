5f662c1962f753fcc20658e125abf9d129d37895
prvthkr <31398919+prvthkr@users.noreply.github.com>
2023-02-12 22:00:21 +0530

    Update ValidParenthesisCheck.C

--- Pranav/Stack/ValidParenthesisCheck.C
+++ Pranav/Stack/ValidParenthesisCheck.C
@@ -1,58 +1,5 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdlib.h>
-
-typedef struct stack {
-    char ch;
-    struct stack *next;
-} Stack;
-
-bool isEmpty(Stack *Head) {
-    if (Head == NULL) {
-        return true;
-    }
-    return false;
-}
-
-void push (Stack **Head, char ch) {
-    Stack *node = (Stack *) malloc(sizeof(Stack));
-    
-    node->ch = ch;
-    node->next = (*Head);
-    (*Head) = node;
-}
-
-char pop(Stack **Head) {
-    
-    if (isEmpty(*Head)) {
-        return -1;
-    }
-
-    char tmp = (*Head)->ch;
-    
-    Stack *node = (*Head);
-    (*Head) = (*Head)->next;
-    
-    free(node);
-    return tmp;
-} 
-
-char top(Stack *Head) {
-    if (isEmpty(Head)) {
-        return -1;
-    }
-    return Head->ch;
-}
-
-void display(Stack *Head) {
-    while(Head) {
-        printf("%c ", Head->ch);
-        Head = Head->next;
-    }
-    printf("\n");
-}
+#define CHAR 1
+#include "stack.h"
 
 bool valid_par(char *str) {
     Stack *Head = NULL;
@@ -74,7 +21,6 @@ bool valid_par(char *str) {
 }
 
 int main() {
-    // Write C code here
     if (valid_par("(())")) {
         printf("true");    
     } else {
